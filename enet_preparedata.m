a50456cdefe1c913402b8ac03bb9d83d5d8f40ba
Mats van Es <m.vanes@donders.ru.nl>
2019-08-08 17:02:22 +0200

    optionably prewhiten data.

--- enet_preparedata.m
+++ enet_preparedata.m
@@ -1,13 +1,34 @@
-function [traindata, testdata, traindesign, testdesign] = enet_preparedata(data, trlnum, tpoint)
+function [traindata, testdata, traindesign, testdesign] = enet_preparedata(data, trlnum, tpoint, do_prewhiten, cov)
+if ~exist('do_prewhiten', 'var'), do_prewhiten = false; end
 
+ntrl = size(data{1}.trial,1);
+ 
+cfg=[];
+for k=1:numel(data)
+  cfg.trials = trlnum;
+  cfg.latency = [data{k}.time(tpoint) data{k}.time(tpoint)];
+  testdata{k} = ft_selectdata(cfg, data{k});
+  testdata{k} = testdata{k}.trial;
+  
+  cfg.trials = setdiff(1:ntrl, trlnum);
+  traindata{k} = ft_selectdata(cfg, data{k});
+  traindata{k} = traindata{k}.trial;
+end
+
+if do_prewhiten % prewhiten only based on the training set
+  cov = squeeze(mean(cov(setdiff(1:ntrl, trlnum),:,:),1));
+  cov_inv = cov^-0.5;
+  for k=1:numel(data)
+    traindata{k} = traindata{k}*cov_inv;
+    testdata{k} = testdata{k}*cov_inv;
+  end
+end
+
+traindata = cat(1,traindata{:});
+testdata = cat(1,testdata{:});
 traindesign = [];
 testdesign = [];
-traindata = [];
-testdata = [];
 for k=1:numel(data)
-  ntrl = size(data{k}.trial,1);
-  traindata = [traindata; reshape(data{k}.trial(setdiff(1:ntrl,trlnum),:,tpoint), ntrl-1, [])];
-  testdata = [testdata; reshape(data{k}.trial(trlnum,:,tpoint), numel(trlnum), [])];
-  traindesign = [traindesign; k*ones(ntrl-1,1)];
+  traindesign = [traindesign; k*ones(ntrl-numel(trlnum),1)];
   testdesign = [testdesign; k*ones(numel(trlnum),1)];
 end
