2cf57d7662328f5e29fff827fc3f73c69ddb7fd1
Mats van Es <m.vanes@donders.ru.nl>
2019-08-16 10:30:28 +0200

    remove randomization of trials from function.

--- randavg_trials.m
+++ randavg_trials.m
@@ -1,4 +1,4 @@
-function [data] = randavg_trials(data, ngroups, groupsize, randnum)
+function [data] = randavg_trials(data, ngroups, groupsize)
 % this function averages trials without replacements into ngroups of
 % 'groupsize' trials. Trials will be randomized according to randum
 % beforehand. This function assumes a timelock structure with dimord
@@ -8,7 +8,7 @@ tmpdata = zeros(ngroups, s2, s3);
 
 idx=1;
 for k=1:ngroups
-  tmpdata(k,:,:) = mean(data.trial(randnum(idx:idx+groupsize-1),:,:),1);
+  tmpdata(k,:,:) = mean(data.trial(idx:idx+groupsize-1,:,:),1);
   idx=idx+groupsize;
 end
 
