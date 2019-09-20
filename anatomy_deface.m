946f68b9156e480b1664d49f40179e7d39ae670b
Mats van Es <m.vanes@donders.ru.nl>
2019-09-20 15:35:52 +0200

    improve manual check for correct defacing.

--- anatomy_deface.m
+++ anatomy_deface.m
@@ -6,11 +6,14 @@ mridir = [projectdir, sprintf('results/anatomy/sub%02d/', subj)];
 s = 'mri_ctf';
 mri.transform = eye(4);
 mri = ft_transform_geometry(transform, mri);
-
+tmpmri = mri;
 
 cfg=[];
 cfg.method = 'spm';
 mri = ft_defacevolume(cfg, mri);
-ft_defacevolume([], mri);
+ft_sourceplot([], mri);
+if ~input('defacing correct? (1: yes, 0: no)')
+  mri = ft_defacevolume([], tmpmri);
+end
 
 save([mridir, 'mri_defaced'], 'mri');
