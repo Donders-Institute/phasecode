6a4cef8d6b3209cc4c28732d6136b6de34fbfde1
Mats van Es <m.vanes@donders.ru.nl>
2019-10-22 15:17:05 +0200

    speed up analysis_decoding.m

--- prewhiten_data.m
+++ prewhiten_data.m
@@ -9,8 +9,11 @@ cfg.removemean = 'no';
 cfg.covariance = 'yes';
 cfg.keeptrials = 'yes';
 for k=1:numel(data)
+  cov{k} = timelockprewhiten(data{k}); % this line replaces the following to increase computation speed. 
+  %{
   cov{k} = ft_timelockanalysis(cfg, data{k});
   cov{k} = cov{k}.cov;
+  %}
 end
 cov = mean(cat(4,cov{:}),4);
 cov_avg= squeeze(mean(cov,1)); % FIXME implement a shrinkage transform (Ledoit and Wolf, 2004), to prevent rank-deficiency
