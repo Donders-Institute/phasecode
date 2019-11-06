2372dc705b221ece3694471dbe846c4aa5b543f7
Mats van Es <m.vanes@donders.ru.nl>
2019-11-06 14:20:36 +0100

    change loading of files, and statistics options.

--- analysis_phasic_modulation.m
+++ analysis_phasic_modulation.m
@@ -4,10 +4,11 @@ contrast = 'attended';
 method = 'glm_perm'; %cosinefit_perm, glm_perm
 hemis=[1];
 freqs = 4:1:30;%32:2:80];
+npermfiles = 10;
 
 centerphase = [0 1/3 2/3 1 4/3 5/3]*pi-pi;
 resultsdir = [projectdir 'results/collapsephasebin/'];
-
+% resultsdir = 'P:/3011085.02/phasecode/results/collapsephasebin/';
 % loading in data
 for h=hemis
   if (strcmp(contrast, 'attended') || strcmp(contrast, 'unattended'))
@@ -19,19 +20,36 @@ for h=hemis
   % load the decoding results for every frequency
   cnt=1;
   for f=freqs
-    tmp{cnt} = load([resultsdir, sprintf('%s/sub%02d_decoding_%sf%d_%d', contrast, subj, hemi, f)]);
-    tmp{cnt} = mean(tmp{cnt}.accuracy,3);
+    tmp{cnt} = load([resultsdir, sprintf('%s/10/sub%02d_decoding_%sf%d_%d', contrast, subj, hemi, f)]);
+    tmp{cnt} = mean(mean(tmp{cnt}.accuracy,3),1);
     
     % now do the same for random phase bins
-    tmp2{cnt} = load([resultsdir, sprintf('%s/sub%02d_decoding_rand_%sf%d_%d', contrast, subj, hemi, f)]);
-    tmp2{cnt} = mean(tmp2{cnt}.accuracy,4);
+    if npermfiles==1
+      tmp2{cnt,1} = load([resultsdir, sprintf('%s/10/sub%02d_decoding_rand_%sf%d_%d', contrast, subj, hemi, f)]);
+      tmp2{cnt,1} = mean(mean(tmp2{cnt,1}.accuracy,4),2);
+    else
+    for k=1:npermfiles
+      tmp2{cnt,k} = load([resultsdir, sprintf('%s/10/sub%02d_decoding_rand_%sf%d_%d%d', contrast, subj, hemi, f,k)]);
+      tmp2{cnt,k} = mean(mean(tmp2{cnt,k}.accuracy,4),2);
+    end
+    end
     cnt=cnt+1;
   end
-  
+  if size(tmp2,2)>1
+    for k=1:numel(freqs)
+      dum = tmp2(k,:);
+      tmp2b{k} = squeeze(cat(1,dum{:}));
+    end
+  else
+    for k=1:numel(freqs)
+      tmp2b{k} = squeeze(tmp2{k});
+    end
+  end
   % restructuring decoding accuracies
-  acc = squeeze(mean(cat(3,tmp{:}),1));
-  acc_rand = squeeze(mean(cat(4,tmp2{:}),2));
+  acc = cat(1,tmp{:})';
+  acc_rand = cat(3,tmp2b{:});
 end
+clear tmp*
 
 for f=1:numel(freqs)
   % observed data
@@ -46,10 +64,15 @@ end
 cfg=[];
 cfg.tail = 1;
 cfg.clustertail = 1;
-cfg.clusterstatistic = 'max';
-cfg.clusterthreshold = 'nonparametric_individual';
 cfg.clusteralpha = 0.05;
-cfg.dim = [];
-stat = clusterstat(cfg, ampl_rand, ampl);
+cfg.alpha = 0.05;
+cfg.dim = [1 numel(freqs)];
+cfg.feedback = 'yes';
+
+cfg.numrandomization = 'all';
+cfg.clusterstatistic = 'max';
+cfg.clusterthreshold = 'nonparametric_common';
+cfg.multivariate = 'yes';
+stat = clusterstat(cfg, ampl_rand', ampl');
 
 
