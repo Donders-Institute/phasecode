b35ba247bd4331759ad70ea1185715389385b0f2
Mats van Es <m.vanes@donders.ru.nl>
2020-01-21 11:01:52 +0100

    non functional change

--- anatomy_deface.m
+++ anatomy_deface.m
@@ -3,7 +3,6 @@ function [mri] = anatomy_deface(subj, mri, transform)
 datainfo
 mridir = [projectdir, sprintf('results/anatomy/sub%02d/', subj)];
 
-s = 'mri_ctf';
 mri.transform = eye(4);
 mri = ft_transform_geometry(transform, mri);
 tmpmri = mri;
