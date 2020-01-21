2c2a9ef264d813fb8c7d5ead4dded41fedb86094
Mats van Es <m.vanes@donders.ru.nl>
2020-01-21 10:59:12 +0100

    add option for using a 2d cortical sheet sourcemodel. move part of the function to *_script.m

--- analysis_lcmv.m
+++ analysis_lcmv.m
@@ -1,19 +1,18 @@
-function analysis_lcmv(subj)
-
+function [dat, tlck, F, L] = analysis_lcmv(subj, model)
+if nargin<2 || ~exist('model', 'var'), model = '3d'; end
 datainfo;
 
 %% load anatomical data
 mridir = [projectdir, sprintf('results/anatomy/sub%02d/', subj)];
-load([mridir, 'sourcemodel3d'])
+if strcmp(model, '2d')
+    load([mridir, 'sourcemodel2d'])
+    load atlas_subparc374_8k.mat
+else
+    load([mridir, 'sourcemodel3d'])
+end
 load([mridir, 'headmodel'])
 headmodel = ft_convert_units(headmodel, 'm');
 sourcemodel = ft_convert_units(sourcemodel, 'm');
-
-% index of maximal gamma power increase
-load([projectdir, sprintf('results/freq/sub%02d_dics_gamma', subj)], 'maxidx', 'Tval')
-if ~isequal(Tval.pos, sourcemodel.pos)
-  error('maxidx does not correspond to sourcemodel positions')
-end
 
 
 %% load MEG data
@@ -29,30 +28,28 @@ end
 fs = dat{1}.fsample;
 
 
-for k=1:numel(dat)
-  data = dat{k};
-  
+for k=1:numel(dat)  
   cfg         = [];
   cfg.latency = [-0.25+1/fs 1.2];
-  data        = ft_selectdata(cfg, data);
+  dat{k}        = ft_selectdata(cfg, dat{k});
   
   cfg                        = [];
   cfg.preproc.demean         = 'yes';
   cfg.preproc.baselinewindow = [-0.1+1/fs 0];
   cfg.removemean             = 'no';
   cfg.keeptrials             = 'yes';
-  tlck                       = ft_timelockanalysis(cfg, data); % use this for virtual channel
+  tlck{k}                       = ft_timelockanalysis(cfg, dat{k}); % use this for virtual channel
   cfg.covariance             = 'yes';
   cfg.keeptrials             = 'no';
   cfg.latency                = [-0.1+1/fs 1];
-  tlck_cov                   = ft_timelockanalysis(cfg, data); % use this for lcmv
+  tlck_cov                   = ft_timelockanalysis(cfg, dat{k}); % use this for lcmv
   
   % prepare anatomical data with session specific leadfields
   cfg                = [];
   cfg.headmodel      = headmodel;
   cfg.grid           = sourcemodel;
-  cfg.grad           = ft_convert_units(tlck.grad, 'm');
-  cfg.channel        = tlck.label;
+  cfg.grad           = ft_convert_units(tlck{k}.grad, 'm');
+  cfg.channel        = tlck{k}.label;
   sourcemodel_ses{k} = ft_prepare_leadfield(cfg);
   
   cfg                    = [];
@@ -68,35 +65,14 @@ for k=1:numel(dat)
   cfg.lcmv.weightnorm    = 'unitnoisegain';
   source                 = ft_sourceanalysis(cfg, tlck_cov);
   
-  sesF      = zeros(size(source.pos,1),numel(tlck_cov.label));
+  sesF      = zeros(size(source.pos,1), numel(tlck_cov.label));
   sesF(source.inside,:) = cat(1,source.avg.filter{:});
   F{k} = sesF;
   
   sesL      = zeros(numel(tlck_cov.label), size(source.pos,1));
   sesL(:,source.inside) = cat(2,source.leadfield{:});
   L{k} = sesL;
-  
-  tmpvirtualchan = keepfields(tlck, {'time', 'dimord','trialinfo', 'sampleinfo'});
-  tmpvirtualchan.label{1} = 'maxgamma';
-  tmpvirtualchan.trial = zeros(size(tlck.trial,1),1,size(tlck.trial,3));
-  for t = 1:size(tlck.trial,3)
-    tmpvirtualchan.trial(:,1,t) = transpose(sesF(maxidx, :)*tlck.trial(:,:,t)');
-  end
-  
-  if k==1
-    virtualchan = tmpvirtualchan;
-  else
-    virtualchan.trial = [virtualchan.trial; tmpvirtualchan.trial];
-    virtualchan.trialinfo = [virtualchan.trialinfo; tmpvirtualchan.trialinfo];
-    virtualchan.sampleinfo = [virtualchan.sampleinfo; tmpvirtualchan.sampleinfo];    
-  end 
 end
-
-save([projectdir, sprintf('results/tlck/sub%02d_virtualchan', subj)], 'virtualchan', 'F', 'L')
-
-
-
-
 
 
 
---
+++ analysis_lcmv_script.m
@@ -0,0 +1,34 @@
+datainfo;
+if ~exist('do_virtualchan', 'var'), do_virtualchan = false; end
+if ~exist('do_lcmvparc', 'var'), do_lcmvparc = false; end
+
+if do_virtualchan
+[data, tlck, F, L] = analysis_lcmv(subj, '3d');
+% index of maximal gamma power increase
+load([projectdir, sprintf('results/freq/sub%02d_dics_gamma', subj)], 'maxidx', 'Tval')
+[virtualchan, F, L] = analysis_virtualchan(tlck, L, F, maxidx);
+
+save([projectdir, sprintf('results/tlck/sub%02d_virtualchan', subj)], 'virtualchan', 'F', 'L')
+end
+
+if do_lcmvparc
+    [data, tlck, F, L] = analysis_lcmv(subj, '2d');
+    load atlas_subparc374_8k.mat
+    for k=1:numel(data)
+        data{k}.trial = permute(cat(3, data{k}.trial{:}), [3 1 2]);
+        data{k}.time = data{k}.time{1};
+        source_parc{k} = lcmvparc(data{k}, tlck{k}, F{k}, L{k}, atlas);
+    end
+    % align over sessions
+    for k=2:numel(source_parc)
+        for ch=1:numel(source_parc{1}.label)
+           r = corr(source_parc{1}.avg(ch,:)', source_parc{k}.avg(ch,:)'); 
+           if r<0
+               source_parc{k}.avg(ch,:) = -source_parc{k}.avg(ch,:);
+               source_parc{k}.F(ch,:) = -source_parc{k}.F(ch,:);
+           end
+        end
+    end
+    save([projectdir, sprintf('results/tlck/sub%02d_sourceparc.mat', subj)], 'source_parc')
+end
+  
