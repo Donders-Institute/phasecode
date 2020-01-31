290d0ad7e5fd057c8df06294de06db5bc71cb030
Mats van Es <m.vanes@donders.ru.nl>
2020-01-31 13:46:28 +0100

    new version.

--- analysis_decoding_weights.m
+++ analysis_decoding_weights.m
@@ -3,30 +3,12 @@
 
 datainfo;
 % choose variables
-subj=4;
-hemi = 1;
-f = 10;
+subj=5;
+hemis = [1 2];
+freqs = 4:1:20;
 contrast = 'attended';
 
 %% load data
-filename = [projectdir 'results/collapsephasebin/'];
-switch contrast
-  case 'congruent'
-    filename = [filename, 'congruent/'];
-  case 'attended'
-    filename = [filename, 'attended/'];
-  case 'unattended'
-    filename = [filename, 'unattended/'];
-end
-filename = [filename, sprintf('sub%02d/sub%02d_decoding', subj, subj)];
-
-if strcmp(contrast, 'attended') || strcmp(contrast, 'unattended')
-  filename = [filename, sprintf('_hemi%d', hemi)];
-end
-filename = [filename, sprintf('_f%d', f)];
-load(filename)
-
-
 cfg=[];
 cnt=1;
 for ses=subjects(subj).validsessions
@@ -37,102 +19,80 @@ for ses=subjects(subj).validsessions
 end
 data = ft_appenddata([], tmp{:});
 
-%% create FT structure
-dat.time = 1:18;
-dat.label = data.label;
-dat.dimord = 'chan_time';
-dat.avg = squeeze(mean(mean(primal_P,4),2));
+filename = [projectdir 'results/collapsephasebin/'];
+switch contrast
+  case 'congruent'
+    filename = [filename, 'congruent/'];
+  case 'attended'
+    filename = [filename, 'attended/'];
+  case 'unattended'
+    filename = [filename, 'unattended/'];
+end
+filename_orig = [filename, sprintf('sub%02d/primalp/sub%02d_decoding', subj, subj)];
+ii=0;
+for f = freqs
+  ii=ii+1;
+  for h=hemis
+    
+if strcmp(contrast, 'attended') || strcmp(contrast, 'unattended')
+  filename = [filename_orig, sprintf('_hemi%d', h)];
+end
+filename = [filename, sprintf('_f%d', f)];
+load(filename)
+
+
+% create FT structure
 % observed that the topographies over folds and over permutations are
 % qualitatively very similar. Over bins there is more variability. No
 % interpretable topo though.
+
+dat{h}.time = freqs;
+dat{h}.label = data.label;
+dat{h}.dimord = 'chan_time';
+dat{h}.avg(:,ii) = mean(mean(mean(primal_P,4),3),2);
+
+primP{h}(:,:,ii) = squeeze(mean(mean(primal_P,4),2));
+  end
+end
+
+cfg=[];
+cfg.cosinefit.statistic = 'complex';
+nbin = 18;
+centerphase = [0:2/nbin:(nbin-1)/(nbin/2)]*pi-pi;
+for h=hemis
+  [s1, s2, s3] = size(primP{h});
+  s{h} = statfun_cosinefit(cfg, reshape(permute(primP{h}, [1 3 2]), [], s2), centerphase);
+  dat{h}.stat = reshape(abs(s{h}.stat), [s1, s3]);
+end
+
 
 % plot
 cfgp=[];
 cfgp.layout = 'CTF275_helmet.mat';
 cfgp.parameter = 'avg';
-figure; ft_topoplotER(cfgp, dat)
+figure; ft_topoplotER(cfgp, dat{:})
+cfgp.parameter = 'stat';
+figure; ft_topoplotER(cfgp, dat{:})
 
 %% Project to source level (2D)
 x = load([projectdir, sprintf('results/tlck/sub%02d_sourceparc.mat', subj)]);
-F = x.source_parc{hemi}.F;
 
 load atlas_subparc374_8k.mat
 load cortex_inflated_shifted.mat
 atlas.pos=ctx.pos;
 exclude_label = match_str(atlas.parcellationlabel, {'L_???_01', 'L_MEDIAL.WALL_01', 'R_???_01', 'R_MEDIAL.WALL_01'});
 selparc = setdiff(1:numel(atlas.parcellationlabel),exclude_label); % hard coded exclusion of midline and ???
-
-source=dat;
-source.brainordinate = atlas;
-source.label = atlas.parcellationlabel;
-source.avg = zeros(374,18);
-source.avg(selparc,:) = F*dat.avg;
+for h=hemis
+  F{h} = x.source_parc{h}.F;
+  source{h}.brainordinate = atlas;
+  source{h}.label = atlas.parcellationlabel;
+  source{h}.avg = zeros(374,numel(freqs));
+  source{h}.avg(selparc,:) = F{h}*dat{h}.avg;
+  source{h}.stat(selparc,:) = F{h}*dat{h}.stat;
+  source{h}.time = freqs;
+end
 
 cfgp2=rmfield(cfgp,'parameter');
 cfgp2.funparameter = 'avg';
 ft_sourcemovie(cfgp2, source);
 
-%% cosinefit on svm weights
-cfg
-cfg=[];
-cfg.cosinefit.statistic = 'complex';
-s = statfun_cosinefit(cfg, source.avg, centerphase);
-
-source2=source;
-source2.time=0;
-source2.avg = abs(s.stat);
-
-cfgp2=cfgp;
-cfgp2.method = 'surface';
-cfgp2.funparameter = 'avg';
-ft_sourceplot(cfgp2, source2);
-
-%% Contrast with those of random permutations
-
-filename = [projectdir 'results/collapsephasebin/'];
-switch contrast
-case 'congruent'
-filename = [filename, 'congruent/'];
-case 'attended'
-filename = [filename, 'attended/'];
-case 'unattended'
-filename = [filename, 'unattended/'];
-end
-filename = [filename, sprintf('sub%02d/primalp/sub%02d_decoding_rand', subj, subj)];
-if strcmp(contrast, 'attended') || strcmp(contrast, 'unattended')
-filename = [filename, sprintf('_hemi%d', hemi)];
-end
-filename = [filename, sprintf('_f%d', f)];
-
-for k=1:10
-x{k} = load([filename, sprintf('_%d',k )], 'primal_P');
-x{k}=x{k}.primal_P;
-end
-
-x=cat(2,x{:});
-x=(cat(5,x{:}));
-x=squeeze(mean(mean(x,4),2));
-
-x=reshape(x,270,[]);
-x=F*x;
-x=reshape(x, [370,18,100]);
-
-for k=1:100
-tmp = statfun_cosinefit(cfg, x(:,:,k), centerphase);
-srand(:,k) = abs(tmp.stat);
-end
-
-source3 = source2;
-source3.avg(selparc,:) = (source3.avg(selparc,:)-mean(srand,2))./std(srand,[],2);
-ft_sourceplot(cfgp2, source3);
-
-
-
-%% Project to source level (3D)
-x=load([projectdir, sprintf('results/freq/sub%02d_dics_gamma', subj)])
-Fdics=x.dicsfilter{hemi};
-Fdics = cat(1,Fdics{:});
-
-load standard_sourcemodel3d6mm.mat
-sourcemodel.avg = zeros(size(sourcemodel.inside));
-sourcemodel.avg(sourcemodel.inside,1) = mean(Fdics*dat.avg,2);
