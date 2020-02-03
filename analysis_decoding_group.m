c754a79cac75cde085889cec7924110b922c27eb
Mats van Es <m.vanes@donders.ru.nl>
2020-02-03 14:26:10 +0100

    use dependent samples test.

--- analysis_decoding_group.m
+++ analysis_decoding_group.m
@@ -28,23 +28,28 @@ end
 
 cfgs = [];
 cfgs.method = 'analytic';
-cfgs.statistic = 'indepsamplesT';
-cfgs.design = [ones(1,numel(valid_subjects)), 2*ones(1,numel(valid_subjects))];
+cfgs.statistic = 'depsamplesT';
+cfgs.design = [ones(1,numel(valid_subjects)), 2*ones(1,numel(valid_subjects)); 1:numel(valid_subjects), 1:numel(valid_subjects)];
+cfgs.parameter = 'trial';
+cfgs.alpha = 0.05/sum([numel(hemis), numel(contrasts)]);
+cfgs.ivar = 1;
+cfgs.uvar = 2;
 
 for c=1:numel(contrasts)
   for h=1:numel(hemis)
-    dat(c,h) = [];
     dat(c,h).dimord = 'rpt_chan_time';
     dat(c,h).label = {'accuracy'};
     dat(c,h).time=0;
-    dat(c,h).trial = mean(acc(c,h,:,:),4);
+    dat(c,h).trial = squeeze(mean(acc(c,h,:,:),4));
     
     datrand(c,h) = dat(c,h);
-    datrand(c,h).trial = mean(accrand(c,h,:,:),4);
+    datrand(c,h).trial = squeeze(mean(accrand(c,h,:,:),4));
     
     stat(c,h) = ft_timelockstatistics(cfgs, dat(c,h), datrand(c,h));
   end
 end
+
+save([projectdir, 'stat_decoding.m'])
 
 
 
