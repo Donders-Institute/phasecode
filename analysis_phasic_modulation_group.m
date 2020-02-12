2fa5d4c579ae2050f6922e46e4660e2ab5ccae51
Mats van Es <m.vanes@donders.ru.nl>
2020-02-12 11:24:33 +0100

    change loading in data and add statistics.

--- analysis_phasic_modulation_group.m
+++ analysis_phasic_modulation_group.m
@@ -1,37 +1,44 @@
 function analysis_phasic_modulation_group(varargin)
 
-doparc = ft_getopt(varargin, 'doparc', 'false');
-
+doparc = ft_getopt(varargin, 'doparc', false);
+hemis=[1 2];
 datainfo
 
-for k=valid_subjects
+for subj=valid_subjects
   filename = [projectdir, sprintf('results/modulation/sub%02d_phasicmodulation_decoding', subj)];
   if doparc
     filename = [filename, '_parc'];
   end
-  tmp{k} = load(filename, 'amp', 'amprand', 'ang');
+  tmp = load(filename, 'amp', 'amp_rand', 'ang');
+  for h=hemis
+    amp{h}(:,subj) = tmp.amp(h,:)';
+    amprand{h}(:,subj,:) = squeeze(tmp.amp_rand(h,:,:));
+    ang{h}(:, subj) = tmp.ang(h,:)';
+  end
 end
 
-%FIXME: From here on, do statistics (yet to be implemented)
+nfreq = size(amp{1},1);
 
+% do 2nd level permutation
 cfg=[];
+cfg.numrandomization = 1000;
+cfg.uvar = 2;
+cfg.ivar = 1;
 cfg.tail = 1;
 cfg.clustertail = 1;
 cfg.clusteralpha = 0.05;
 cfg.alpha = 0.05;
-cfg.dim = [1 numel(freqs)];
-cfg.feedback = 'yes';
-
-cfg.numrandomization = 'all';
 cfg.clusterstatistic = 'maxsum';
 cfg.clusterthreshold = 'nonparametric_individual';
-cfg.multivariate = 'yes';
-for k=1:2
-  stat(k) = clusterstat(cfg, squeeze(amp_rand(k,:,:)), amp(k,:)');
+cfg.connectivity = 1;
+cfg.dim = [1 nfreq];
+
+for h=hemis
+  stat(h) = statfun_perm(cfg, amp{h}, amprand{h});
 end
 
 filename = [projectdir, 'results/stat_phasicmodulation_decoding'];
 if doparc
   filename = [filename, '_parc'];
 end
-save(filename, 'stat')
+save(filename, 'stat', 'amp', 'amprand')
