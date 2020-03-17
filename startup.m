f5f70123a8520adeb115050bb4f1ad79b93399ce
Mats van Es <m.vanes@donders.ru.nl>
2020-03-17 17:51:29 +0100

    enable to work with PC or on the cluster.

--- startup.m
+++ startup.m
@@ -1,23 +1,26 @@
 function startup
 
 if ispc
+    defaultdir = 'P:/';
 else
-  addpath('/project/3011085.06/scripts/');
-  addpath('/project/3011085.06/scripts/cellfunction/');
-  addpath('/project/3011085.06/data/');
-  addpath('/project/3011085.06/results/');
-  addpath('/project/3011085.06/scripts/CircStat');
-  addpath('/project/3011085.06/scripts/circshift_columns/')
-  addpath('/project/3011085.06/scripts/fieldtrip');
-  addpath('/project/3011085.06/scripts/fieldtrip/qsub');
-  addpath('/project/3011085.06/scripts/fieldtrip/external/dmlt/');
-  addpath('/project/3011085.06/scripts/fieldtrip/external/dmlt/external/glmnet/')
-  addpath('/project/3011085.06/scripts/fieldtrip/external/dmlt/')
-  addpath('/project/3011085.06/scripts/fieldtrip/external/dmlt/external/')
-  addpath('/project/3011085.06/scripts/fieldtrip/external/dmlt/external/svm/')
-  addpath('/project/3011085.06/scripts/fieldtrip/external/brewermap');
-  cd('/project/3011085.06/scripts/')
+    defaultdir = '/project/';
 end
+addpath([defaultdir, '3011085.06/scripts/']);
+addpath([defaultdir, '3011085.06/scripts/cellfunction/']);
+addpath([defaultdir, '3011085.06/data/']);
+addpath([defaultdir, '3011085.06/results/']);
+addpath([defaultdir, '3011085.06/scripts/CircStat']);
+addpath([defaultdir, '3011085.06/scripts/circshift_columns/'])
+addpath([defaultdir, '3011085.06/scripts/fieldtrip']);
+addpath([defaultdir, '3011085.06/scripts/fieldtrip/qsub']);
+addpath([defaultdir, '3011085.06/scripts/fieldtrip/external/dmlt/']);
+addpath([defaultdir, '3011085.06/scripts/fieldtrip/external/dmlt/external/glmnet/'])
+addpath([defaultdir, '3011085.06/scripts/fieldtrip/external/dmlt/'])
+addpath([defaultdir, '3011085.06/scripts/fieldtrip/external/dmlt/external/'])
+addpath([defaultdir, '3011085.06/scripts/fieldtrip/external/dmlt/external/svm/'])
+addpath([defaultdir, '3011085.06/scripts/fieldtrip/external/brewermap']);
+cd([defaultdir, '3011085.06/scripts/'])
+
 ft_defaults
 
 end
