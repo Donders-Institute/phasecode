e7f6846b4e1b1ff0a5484d36847a41394354e564
Mats van Es <m.vanes@donders.ru.nl>
2020-09-16 17:19:28 +0200

    compute posthoc bayes factors

--- analysis_phasic_modulation_parc_group.m
+++ analysis_phasic_modulation_parc_group.m
@@ -3,7 +3,9 @@
 % cortical sheet.
 clear 
 datainfo;
-whichdata = input('do you want to test modulation in behavior, or in neural data?');
+if ~exist('whichdata', 'var')
+    whichdata = input('do you want to test modulation in behavior, or in neural data?');
+end
 load atlas_subparc374_8k.mat
 exclude_label = match_str(atlas.parcellationlabel, {'L_???_01', 'L_MEDIAL.WALL_01', 'R_???_01', 'R_MEDIAL.WALL_01'});
 selparc = setdiff(1:numel(atlas.parcellationlabel),exclude_label); % hard coded exclusion of midline and ???
@@ -103,8 +105,40 @@ end
 switch whichdata
   case 'behavior'
     filename = [projectdir, 'results/stat_phasicmodulation_behavior'];
-    save(filename, 'stat', 'amp', 'ang','ampr')
+%     save(filename, 'stat', 'amp', 'ang','ampr')
   case 'neural'
     filename = [projectdir, 'results/stat_phasicmodulation_decoding_parc'];
-    save(filename, 'stat', 'amp', 'ang','ampr')
+%     save(filename, 'stat', 'amp', 'ang','ampr')
 end
+
+n=10;
+cfg = [];
+cfg.method = 'analytic';
+cfg.statistic = 'ft_statfun_bayesfactor';
+cfg.ivar = 1;
+cfg.uvar = 2;
+cfg.design = [ones(1,n), 2*ones(1,n); 1:n, 1:n];
+for h=hemis
+    dat{h} = [];
+    dat{h}.label=atlas.parcellationlabel;
+    dat{h}.dimord = 'rpt_chan_freq';
+    dat{h}.powspctrm = permute(amp{h},[3 1 2]);
+    dat{h}.powspctrm(isnan(dat{h}.powspctrm)) = 0;
+    dat{h}.freq = 4:20;
+    
+    datrand{h} = removefields(dat{h}, 'powspctrm');
+    datrand{h}.powspctrm = permute(ampr{h}, [3 1 2]);
+    datrand{h}.powspctrm(isnan(datrand{h}.powspctrm)) = 0;
+    bf_single(h) = ft_freqstatistics(cfg, dat{h}, datrand{h});
+    
+    % bf for largest cluster
+    tmp = reshape(dat{h}.powspctrm, n, []);
+    dat{h}.powspctrm = nanmean(tmp(:, stat{h}.posclusterslabelmat==1),2);
+    dat{h}.freq = 0;
+    dat{h}.label = {'modulation'};
+    tmp = reshape(datrand{h}.powspctrm, n, []);
+    datrand{h} = removefields(dat{h}, 'powspctrm');
+    datrand{h}.powspctrm = nanmean(tmp(:, stat{h}.posclusterslabelmat==1),2);
+    bf(h) = ft_freqstatistics(cfg, dat{h}, datrand{h});
+end
+save(filename, 'bf','bf_single', '-append')
